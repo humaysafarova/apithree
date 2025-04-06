70b4efedb1fc34a9c01961ff44ba9adb73ea1194
humaysafarova <166644215+humaysafarova@users.noreply.github.com>
2025-04-06 15:48:51 +0000

    first commit

---
+++ api/models.py
@@ -0,0 +1,13 @@
+class News:
+
+    def __init__(self, tit, desc, img):
+
+        self.tit = tit
+        self.desc = desc
+        self.img = img  
+    
+news1 = News(
+    "iPhone 16 Pro Max",
+    "The iPhone 16 Pro Max will feature a larger 6.9 inch display, powered by the A18 Pro chip with enhanced AI capabilities, and will include a new dedicated Capture button for improved camera functionality.",
+    
+)
